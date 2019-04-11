5f94ce240c62f48ebcda507e6c4834ac1f9009df
jim white <jbwhite2@ncsu.edu>
2019-04-11 17:33:32 -0400

    gulpfile dev

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('markdown_docker', function() {
 
 gulp.task('move_images', function() {
     return gulp.src("images/tbas-documentation/*.*")
-        .pipe(gulp.dest('/var/www/html/tbas2_1/images/tbas-documentation'));
+        .pipe(gulp.dest('/var/www/html/tbas2_1/pages/images/tbas-documentation'));
 });
 
 gulp.task('default', ['markdown_docker', 'move_images']);
