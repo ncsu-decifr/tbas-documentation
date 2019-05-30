08d63b9ddad36f2059044d1f384a6b389ba674fa
jim white <jbwhite2@ncsu.edu>
2019-05-30 14:01:57 -0400

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -18,5 +18,9 @@ gulp.task('move_images', function() {
     return gulp.src("images/tbas-documentation/*.*")
         .pipe(gulp.dest('/var/www/html/tbas2_1/pages/images/tbas-documentation'));
 });
+gulp.task('move_csv', function() {
+    return gulp.src("documentation.csv")
+        .pipe(gulp.dest('/var/www/html/tbas2_1/pages/'));
+});
 
-gulp.task('default', ['markdown_docker', 'move_images']);
+gulp.task('default', ['markdown_docker', 'move_images', 'move_csv']);
